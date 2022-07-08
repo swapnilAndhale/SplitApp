c6c7260f4bd40abf70b0add5ae4c24f044ec59c4
Swapnil <swapnilandhale3@gmail.com>
2022-07-08 18:19:07 +0530

    changes made

--- Starter/ContentView.swift
+++ Starter/ContentView.swift
@@ -10,30 +10,36 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkValue = 0.0
-    @State private var numberOfPeople = 1
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 30]
     
     var body: some View {
         
         NavigationView {
-        Form {
-            Section {
-                TextField("Selcet Value", value: $checkValue, format: .currency(code: "INR"))
-                    .keyboardType(.decimalPad)
-                
-                TextField("Number of people", value: $numberOfPeople, format: .number)
-                    .keyboardType(.numberPad)
-                
-                Picker("Number of people", selection: $numberOfPeople) {
-                    ForEach(2..<10) {
-                        Text("\($0) People")
+            Form {
+                Section {
+                    TextField("Selcet Value", value: $checkValue, format: .currency(code: "INR"))
+                        .keyboardType(.decimalPad)
+                    
+                    Section {
+                        
+                        TextField("Number of people", value: $numberOfPeople, format: .number)
+                            .keyboardType(.numberPad)
+                        
+                        Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach(2..<10) {
+                                Text("\($0) People")
+                            }
+                        }
+                        .pickerStyle(.automatic)
+                    } header: {
+                        Text("How many number of peoples you want?")
                     }
                 }
-                .pickerStyle(.automatic)
-            } header: {
-                Text("How many number of peoples you want?")
             }
-        }
-        .navigationTitle("SwiftUI Picker")
+            .navigationTitle("SwiftUI Picker")
         }
         
     }
