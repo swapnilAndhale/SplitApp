d8e93188064637a33ef805f7ab5f58b29578111f
Swapnil <swapnilandhale3@gmail.com>
2022-07-08 17:48:51 +0530

    Initial Commit

---
+++ Starter/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Starter
+//
+//  Created by Swapnil on 05/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkValue = 0.0
+    @State private var numberOfPeople = 1
+    
+    var body: some View {
+        
+        NavigationView {
+        Form {
+            Section {
+                TextField("Selcet Value", value: $checkValue, format: .currency(code: "INR"))
+                    .keyboardType(.decimalPad)
+                
+                TextField("Number of people", value: $numberOfPeople, format: .number)
+                    .keyboardType(.numberPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<10) {
+                        Text("\($0) People")
+                    }
+                }
+                .pickerStyle(.automatic)
+            } header: {
+                Text("How many number of peoples you want?")
+            }
+        }
+        .navigationTitle("SwiftUI Picker")
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Swapnil on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
